75834f86a98ace78200b6f29369937cfd1dd087d
Steven Smith <steven.76484@gmail.com>
2019-07-01 22:38:06 -0400

    Updating app script to create imposter and then create locations stub

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const axios = require('axios');
 
-axios.post('http://localhost:2525/imposters', {
-    "port": 4545,
-    "protocol": "http",
-    "stubs": [
-        {
+const MOUNTEBANK_URL = "http://localhost:2525"
+const IMPOSTER_PORT = 4545;
+
+const LOCATIONS = {
+    CHICAGO: {
+        "city": "Chicago",
+        "subdivision_code": "IL"
+    },
+    TORONTO: {
+        "city": "Toronto",
+        "subdivision_code": "ON"
+    },
+    VANCOUVER: {
+        "city": "Vancouver",
+        "subdivision_code": "BC"
+    }
+}
+
+const createImposter = () => {
+    return axios
+        .delete(`${MOUNTEBANK_URL}/imposters`)
+        .then(() => {
+            return axios.post(`${MOUNTEBANK_URL}/imposters`, {
+                "port": IMPOSTER_PORT,
+                "protocol": "http",
+            });
+        });
+};
+
+const createLocationsStub = locations => {
+    return axios.post(`${MOUNTEBANK_URL}/imposters/${IMPOSTER_PORT}/stubs`, {
+        "stub": {
             "predicates": [
                 {
                     "equals": {
@@ -13,38 +40,35 @@ axios.post('http://localhost:2525/imposters', {
                     }
                 }
             ],
-            "responses": [
-                {
+            "responses": locations.map(location => {
+                return {
                     "is": {
                         "statusCode": 200,
                         "headers": {
                             "Content-Type": "application/json"
                         },
                         "body": {
-                            "location": {
-                                "city": "Toronto",
-                                "subdivision_code": "ON"
-                            },
-                            "success": true
-                        }
-                    }
-                },
-                {
-                    "is": {
-                        "statusCode": 200,
-                        "headers": {
-                            "Content-Type": "application/json"
-                        },
-                        "body": {
-                            "location": {
-                                "city": "Chicago",
-                                "subdivision_code": "IL"
-                            },
+                            location,
                             "success": true
                         }
                     }
                 }
-            ]
+            })
         }
-    ]
-});
+    });
+};
+
+createImposter()
+    .then(() => {
+        return createLocationsStub([
+            LOCATIONS.TORONTO,
+            LOCATIONS.VANCOUVER,
+            LOCATIONS.CHICAGO
+        ])
+    })
+    .then(() =>  {
+        console.log("All Locations created.");
+    })
+    .catch(err => {
+        console.log(err);
+    });
