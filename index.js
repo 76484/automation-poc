bfba944e56aecea8bc786d67942ae10ecb772e81
Steven Smith <steven.76484@gmail.com>
2019-07-03 23:52:54 -0400

    Adding ability to iteratively test multiple locations

--- index.js
+++ index.js
@@ -36,70 +36,75 @@ const createImposter = () => {
 };
 
 const createLocationsStub = locations => {
-    return axios.post(`${MOUNTEBANK_URL}/imposters/${IMPOSTER_PORT}/stubs`, {
-        "stub": {
-            "predicates": [
-                {
-                    "equals": {
-                        "path": "/v2/locations",
-                        "method": "GET"
-                    }
-                }
-            ],
-            "responses": locations.map(location => {
-                return {
-                    "is": {
-                        "statusCode": 200,
-                        "headers": {
-                            "Access-Control-Allow-Origin": "*",
-                            "Content-Type": "application/json"
-                        },
-                        "body": {
-                            location,
-                            "success": true
+    return axios.put(`${MOUNTEBANK_URL}/imposters/${IMPOSTER_PORT}/stubs`, {
+        "stubs": [
+            {
+                "predicates": [
+                    {
+                        "equals": {
+                            "path": "/v2/locations",
+                            "method": "GET"
                         }
                     }
-                }
-            })
-        }
+                ],
+                "responses": locations.map(location => {
+                    return {
+                        "is": {
+                            "statusCode": 200,
+                            "headers": {
+                                "Access-Control-Allow-Origin": "*",
+                                "Content-Type": "application/json"
+                            },
+                            "body": {
+                                location,
+                                "success": true
+                            }
+                        }
+                    }
+                })
+            }
+        ]
+    });
+};
+
+const createImposterPromise = createImposter();
+
+const createDriver = () => {
+    return new Promise(resolve => {
+        new Builder()
+            .forBrowser('chrome')
+            .build()
+            .then(driver => resolve(driver));
     });
 };
 
 describe('Location', () => {
-    it('should be set with city and subdivision code from Location response', done => {
-        createImposter()
-            .then(() => {
-                return createLocationsStub([
-                    LOCATIONS.TORONTO,
-                    LOCATIONS.VANCOUVER,
-                    LOCATIONS.CHICAGO
-                ])
-            })
-            .then(() =>  {
-                return new Promise(resolve => {
-                    new Builder()
-                        .forBrowser('chrome')
-                        .build()
-                        .then(driver => resolve(driver));
+    it('should be set with city and subdivision code from Location response', () => {
+        const testLocationText = location => {
+            return createLocationsStub([location])
+                .then(() => createDriver())
+                .then(driver => {
+                    return driver
+                        .get('http://localhost:3000')
+                        .then(() => {
+                            return driver.wait(
+                                until.elementLocated(By.id('Location'), 10 * 1000)
+                            );
+                        })
+                        .then(el => el.getText())
+                        .then(text => {
+                            assert.equal(text, `${location.city}, ${location.subdivision_code}`);
+                        })
+                        .finally(() => {
+                            driver.quit();
+                        });
                 });
-            })
-            .then(driver => {
-                return driver
-                    .get('http://localhost:3000')
-                    .then(() => {
-                        return driver.wait(
-                            until.elementLocated(By.id('Location'), 10 * 1000)
-                        );
-                    })
-                    .then(el => el.getText())
-                    .then(text => {
-                        assert.equal(text, 'Toronto, ON');
-                        done()
-                    })
-                    .finally(() => {
-                        driver.quit();
-                    });
-            })
-            .catch(err => done(err));
+        };
+
+        return createImposterPromise
+            .then(() => testLocationText(LOCATIONS.TORONTO))
+            .then(() => testLocationText(LOCATIONS.VANCOUVER))
+            .then(() => testLocationText(LOCATIONS.CHICAGO))
+        ;
     });
 });
