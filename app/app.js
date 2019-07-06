121dcf3121c4ea1fe016cb3e6cf9717b3190ec95
Steven Smith <steven.76484@gmail.com>
2019-07-06 18:41:32 -0400

    Adding scripts to run app as Partner A or Partner B

--- app/app.js
+++ app/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const partner = process.env.partner || "A"
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app for partner ${partner} listening on port ${port}!`));
